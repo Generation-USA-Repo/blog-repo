b6db1332b654be99ab57a06ac6e7346a01ef9732
Danny-JaJr <dlaf@qlead.io>
2023-05-31 17:30:38 -0400

    readme.md edited

--- README.md
+++ README.md
@@ -4,20 +4,32 @@
 
 App builts with Javascript HTML and CSS that store pictures locally, where users can store, view , update and delete pictures or descriptions
 
-## Social Network
-View saved pictures and rewatch your memories
+## Social Network images storage 
+view saved pictures and rewatch your memories
 
 
 
 
 ## Navigation
 
-* There should be at minimum 4 pages: (your pages can be different, these are just examples)
-  * Main page (Home)  - View some pics
-  * About Us - Staic information about navigation
-  * My Library / Posts add picture 
-  * Delete Page view and delete picture
+* CRUD Navaigation create on 4 pages using local Storage and JavaScript DOM queries
+  * About page (Home)  - Navigation plus view some Cards
+  * Edit page          - Navigation plus edit Card
+  * Upload page        - Navigation plus add image through a form
+  * Delete page        - Navigation plus delete card
 
-## Example
+## About Page
+![About Page](./pictures/about.PNG)
 
-![](https://google.com)
+## Upload Page
+![About upload](./pictures/upload.PNG)
+
+
+## Edit Page
+![About Edit](./pictures/edit.PNG)
+
+
+## Delete Page
+![About Delete](./pictures/delete.PNG)
+
+
--- about.js
+++ about.js
@@ -1,19 +1,82 @@
-// Get the element to display the images
-const galleryContainer = document.getElementById('content');
+// Array to store uploaded images
+let images = [];
 
-// Function to display images in the gallery
-function displayImages() {
-  galleryContainer.innerHTML = '';
+// Get form elements
+const form = document.getElementById('upload-form');
+const imageInput = document.getElementById('image-input');
+const nameInput = document.getElementById('name-input');
+const descriptionInput = document.getElementById('description-input');
 
-  for (let i = 0; i < images.length; i++) {
-    const image = images[i];
-    const card = createCard(image);
-    galleryContainer.appendChild(card);
+// Load images from localStorage
+function loadImagesFromLocalStorage() {
+  const savedImages = localStorage.getItem('images');
+  if (savedImages) {
+    images = JSON.parse(savedImages);
+    displayImages();
   }
 }
+
+// Display images in the gallery
+function displayImages() {
+  const gallery = document.getElementById('image-gallery');
+  const pagination = document.getElementById('pagination');
+  gallery.innerHTML = '';
+  pagination.innerHTML = '';
+
+  const itemsPerPage = 10;
+  const totalPages = Math.ceil(images.length / itemsPerPage);
+
+  let currentPage = 1;
+  if (location.hash) {
+    const hashValue = parseInt(location.hash.substring(1));
+    if (!isNaN(hashValue) && hashValue >= 1 && hashValue <= totalPages) {
+      currentPage = hashValue;
+    }
+  }
+  
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+
+  for (let i = startIndex; i < endIndex && i < images.length; i++) {
+    const image = images[i];
+    const card = createCard(image);
+    gallery.appendChild(card);
+  }
+
+  if (totalPages > 1) {
+    for (let i = 1; i <= totalPages; i++) {
+      const button = document.createElement('button');
+      button.innerText = i;
+      button.addEventListener('click', function() {
+        location.hash = i;
+        displayImages();
+      });
+      pagination.appendChild(button);
+    }
+  }
+}
+
+// Create a card for an image
+function createCard(image) {
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  const img = document.createElement('img');
+  img.src = image.data;
+  card.appendChild(img);
+
+  const name = document.createElement('h3');
+  name.innerText = image.name;
+  card.appendChild(name);
+
+  const description = document.createElement('p');
+  description.innerText = image.description;
+  card.appendChild(description);
+  return card;
+}
+
 
 // Load saved images on page load
 document.addEventListener('DOMContentLoaded', function() {
   loadImagesFromLocalStorage();
-  displayImages();
 });
