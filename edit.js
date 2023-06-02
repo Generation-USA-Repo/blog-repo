84c4737c0939aa799d47e5a702f98fe3601a4490
Danny-JaJr <dlaf@qlead.io>
2023-06-02 12:37:33 -0400

    edit and navbar fixed

--- edit.js
+++ edit.js
@@ -92,24 +92,60 @@ function createCard(image) {
     editImage(image);
   });
   cardBody.appendChild(editButton);
-
   card.appendChild(cardBody);
   return card;
 }
 
 
-// Edit an image
 function editImage(image) {
-  const newName = prompt('Enter a new name:', image.name);
-  if (newName !== null) {
-    const newDescription = prompt('Enter a new description:', image.description);
-    if (newDescription !== null) {
-      image.name = newName;
-      image.description = newDescription;
-      saveImagesToLocalStorage();
-      displayImages();
-    }
-  }
+  const popup = document.createElement('div');
+  popup.innerHTML =`<div class="container">
+  <div>
+      <h2>Add New Info</h2>
+      <form id="editForm">
+          <div >
+          <label for="newName">Edit:</label>
+          </div>
+          <div >
+               <input type="text" id="newName" value="${image.name}" placeholder="New Name"><br><br>
+          </div>
+          <div >
+              <input type="text" id="newDescription" value="${image.description}" placeholder=New Description"><br><br>
+          </div>
+          <button type="submit" class="btn btn-success">Save</button>
+          <button type="button" class="btn btn-warning" id="cancelButton">Cancel</button>
+        </form>
+      </form>
+  </div>`;
+
+  popup.style.position = 'fixed';
+  popup.style.top = '50%';
+  popup.style.left = '50%';
+  popup.style.transform = 'translate(-50%, -50%)';
+  popup.style.padding = '20px';
+  popup.style.background = 'white';
+  popup.style.border = '1px solid black';
+
+  document.body.appendChild(popup);
+
+  const form = document.getElementById('editForm');
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const newName = document.getElementById('newName').value;
+    const newDescription = document.getElementById('newDescription').value;
+    // const newImageFile = document.getElementById('newImage').files[0];
+  
+    image.name = newName;
+    image.description = newDescription;
+    saveImagesToLocalStorage();
+    displayImages();
+    document.body.removeChild(popup);
+  });
+  // cancel the edit form
+  const cancelButton = document.getElementById('cancelButton');
+  cancelButton.addEventListener('click', () => {
+    document.body.removeChild(popup);
+  });
 }
 
 
